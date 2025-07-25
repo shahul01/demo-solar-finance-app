8eadfbae4e04567acc3ce324deba9d1ea4067e0b
shahul01 <shahulhammeed01@gmail.com>
2025-07-25 06:24:28 +0530

    dev: updated drizzle config

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,12 +2,7 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
-import { building } from '$app/environment';
 
-// Skip database initialization during build
-if (building) {
-	throw new Error('Database should not be accessed during build');
-}
 
 let db: ReturnType<typeof drizzle> | null = null;
 
